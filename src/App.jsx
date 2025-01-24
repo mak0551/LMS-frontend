708c6644488d99e3629b90817a5fef17e247dbe2
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-01-24 18:27:45 +0530

    landingpage done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="bg-green-50">
       <Router>
         <Navbar />
         <Routes>
