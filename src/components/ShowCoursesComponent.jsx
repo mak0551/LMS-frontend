ade9867baeac02b085d3241a1f8fc9954f84cdc6
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-02-24 14:35:57 +0530

    showcoursescomponent

--- src/components/ShowCoursesComponent.jsx
+++ src/components/ShowCoursesComponent.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function ShowCoursesComponent({data}) {
+function ShowCoursesComponent({ data }) {
   return (
-    <div className="mx-40 py-12">
+    <div className="sm:mx-40 mx-4 py-12">
       {data.length > 0 ? (
         data.map((data, index) => (
           <div
-            className="w-full h-[300px] flex rounded-lg overflow-hidden my-8 bg-zinc-100 font-mono p-6"
+            className="w-full sm:h-[300px] md:h-fit sm:flex flex-col rounded-lg overflow-hidden my-8 bg-white hover:bg-zinc-50 font-mono p-6 shadow-md hover:shadow-xl transition-all duration-300 group"
             key={index}
           >
-            <div className="w-[40%]">
+            <div className="sm:w-[40%] md:w-full overflow-hidden rounded-md">
               <img
                 src={`${
                   data.thumbNail
@@ -18,21 +18,21 @@ function ShowCoursesComponent({data}) {
                     : "https://plus.unsplash.com/premium_photo-1685086785636-2a1a0e5b591f?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 }`}
                 alt=""
-                className="h-full w-full rounded-md hover:brightness-125"
+                className="h-full w-full rounded-md transition-transform duration-300 group-hover:scale-105"
               />
             </div>
-            <div className=" w-[60%] px-8 relative">
-              <h1 className="mt-4 mb-2 text-3xl capitalize font-mono font-bold">
+            <div className=" sm:w-[60%] md:w-full px-8 relative">
+              <h1 className="mt-4 mb-2 text-3xl capitalize font-mono font-bold ">
                 {data.title}
               </h1>
-              <p className="text-zinc-500 mb-4 text-sm">{data.description}</p>
+              <p className="text-zinc-500 mb-4 text-sm truncate w-3/4">{data.description}</p>
               <div className="capitalize">
                 {data.teacher?.name ? `By ${data.teacher.name}` : ""}
               </div>
               <div className="text-xs mb-2 font-base">
                 Last Updated {data.updatedAt.slice(0, 10)}
               </div>
-              <span className="absolute top-4 right-10 text-xl">
+              <span className="absolute sm:top-4 sm:right-10 right-4 top-10 text-xl">
                 &#8377;{data.price}
               </span>
 
