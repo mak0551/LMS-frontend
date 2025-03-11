41b19286576efff5887d81caf4bd8911732df1cd
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-03-11 20:14:59 +0530

    added loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-fit bg-white">
+      <div className="w-16 h-16 border-4 border-white border-t-black rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/state_management/AuthContext.jsx
+++ src/state_management/AuthContext.jsx
@@ -15,6 +15,8 @@ export const AuthProvider = ({ children }) => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
+    } else {
+      setUser(false);
     }
   }, []);
 
