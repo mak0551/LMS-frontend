28695919f7798675fb1debcf3a812bfd1378f31a
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-03-15 21:03:51 +0530

    my course authentication

--- src/components/dashboard/MyCourses.jsx
+++ src/components/dashboard/MyCourses.jsx
@@ -1,10 +1,23 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Overview from "./components/overView/Overview";
 import ManageCourses from "./components/manage/ManageCourses";
+import { useAuth } from "../../state_management/AuthContext";
+import { toast } from "react-toastify";
 
 export default function MyCourses() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user === null) {
+      return;
+    }
+    if (user === false) {
+      toast.error("please login to view your learnings");
+      navigate("/signin");
+    }
+  }, [user]);
   return (
     <div className="flex overflow-hidden overflow-x-auto">
       <Sidebar />
