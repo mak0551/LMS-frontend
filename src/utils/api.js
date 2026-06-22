5ab492ccd05bc3001cef44664f090cd231866080
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2026-06-22 18:43:18 +0530

    baseurl changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:4040",
-  //   baseURL: "https://lms-htvh.onrender.com",
+  // baseURL: "http://localhost:4040",
+    baseURL: "https://lms-htvh.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
